9ba9f44d72a683c21599da3e8c111598b5f1a005
Yukine <devyukine@gmx.de>
2020-10-23 16:51:38 +0200

    feat(index): add ParsedFileInfo

--- src/index.rs
+++ src/index.rs
@@ -1,3 +1,5 @@
+use crate::gdrive::FileInfo;
+use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -25,5 +27,26 @@ impl Index {
 impl FileEntry {
     pub fn new(url: String, size: u64) -> FileEntry {
         FileEntry { url, size }
+    }
+}
+
+pub struct ParsedFileInfo {
+    pub id: String,
+    pub size: String,
+    pub name: String,
+    pub name_encoded: String,
+    pub shared: bool,
+}
+
+impl ParsedFileInfo {
+    pub fn new(info: FileInfo) -> ParsedFileInfo {
+        let name_encoded = utf8_percent_encode(&*info.name.clone(), NON_ALPHANUMERIC).to_string();
+        ParsedFileInfo {
+            id: info.id,
+            size: info.size,
+            name: info.name,
+            name_encoded,
+            shared: info.shared,
+        }
     }
 }
