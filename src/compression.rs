89cb1f18a3ea4e1e05e3b157bfd537b56e4eb395
Yukine <devyukine@gmx.de>
2020-10-27 13:30:55 +0100

    feat(compression): add Display impl for CompressionFlag

--- src/compression.rs
+++ src/compression.rs
@@ -1,4 +1,5 @@
 use crate::result::Result;
+use core::fmt;
 use flate2::write::ZlibEncoder;
 use flate2::Compression;
 use std::io::Write;
@@ -22,6 +23,20 @@ impl FromStr for CompressionFlag {
             "zlib" => Ok(CompressionFlag::Zlib),
             _ => Err(format!("Invalid compression: {}", compression)),
         }
+    }
+}
+
+impl fmt::Display for CompressionFlag {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                CompressionFlag::Off => "off".to_string(),
+                CompressionFlag::ZSTD => "zstd".to_string(),
+                CompressionFlag::Zlib => "zlib".to_string(),
+            }
+        )
     }
 }
 
